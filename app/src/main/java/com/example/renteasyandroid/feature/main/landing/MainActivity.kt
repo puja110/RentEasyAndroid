af2323d6e466cabba9185e80abee86db0301fa14
puja110 <csta.puja@gmail.com>
2023-10-22 23:22:34 -0400

    bottom navigation added in main screen

--- app/src/main/java/com/example/renteasyandroid/feature/main/landing/MainActivity.kt
+++ app/src/main/java/com/example/renteasyandroid/feature/main/landing/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.renteasyandroid.feature.main.landing
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.window.OnBackInvokedDispatcher
+import androidx.fragment.app.Fragment
 import com.example.renteasyandroid.R
 import com.example.renteasyandroid.base.BaseActivity
 import com.example.renteasyandroid.databinding.ActivityMainBinding
@@ -24,6 +26,41 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setDefaultFragment(savedInstanceState)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_home -> {
+                    val fragment = HomeFragment.getInstance()
+                    addFragment(fragment)
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.action_favourite -> {
+                    val fragment = HomeFragment.getInstance()
+                    addFragment(fragment)
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.action_add -> {
+                    val fragment = HomeFragment.getInstance()
+                    addFragment(fragment)
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.action_history -> {
+                    val fragment = HomeFragment.getInstance()
+                    addFragment(fragment)
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.action_profile -> {
+                    val fragment = HomeFragment.getInstance()
+                    addFragment(fragment)
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 
     override fun initObservers() {
@@ -37,5 +74,22 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
                     HomeFragment.getInstance()
                 ).commit()
         }
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, fragment, fragment.javaClass.simpleName)
+            .commit()
+    }
+
+    override fun getOnBackInvokedDispatcher(): OnBackInvokedDispatcher {
+        if (binding.bottomNavigationView.selectedItemId == R.id.action_home) {
+            onBackPressedDispatcher.onBackPressed()
+            finish()
+        } else {
+            binding.bottomNavigationView.selectedItemId = R.id.action_home
+        }
+
+        return super.getOnBackInvokedDispatcher()
     }
 }
