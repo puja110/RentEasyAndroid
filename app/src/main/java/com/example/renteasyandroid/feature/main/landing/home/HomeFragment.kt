5cba20bf0927fd0bd0b1a450cf3c9effca17ccb6
puja110 <csta.puja@gmail.com>
2024-04-08 19:35:05 -0400

    search functionality implemented with firebase

--- app/src/main/java/com/example/renteasyandroid/feature/main/landing/home/HomeFragment.kt
+++ app/src/main/java/com/example/renteasyandroid/feature/main/landing/home/HomeFragment.kt
@@ -98,7 +98,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
                 Status.COMPLETE -> {
                     binding.progressBar.visibility = View.GONE
                     response.data?.let {
-                        rAdapter = RecentlyUpdatedAdapter(it.toMutableList()) { response, favorite ->
+                        rAdapter = RecentlyUpdatedAdapter(it.toMutableList()) { response ->
                             Log.d(TAG, "observeRecentlyUpdatedResponse: ${response.propertyName}")
                             RentDetailActivity.start(
                                 requireActivity(),
