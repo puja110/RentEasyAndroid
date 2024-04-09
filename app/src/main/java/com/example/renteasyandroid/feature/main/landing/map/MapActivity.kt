0d045e2f5e3835597dc1dcb5bc666d454e959e47
puja110 <csta.puja@gmail.com>
2024-04-09 15:11:20 -0400

    navigation intent from detail to map

--- app/src/main/java/com/example/renteasyandroid/feature/main/landing/map/MapActivity.kt
+++ app/src/main/java/com/example/renteasyandroid/feature/main/landing/map/MapActivity.kt
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
